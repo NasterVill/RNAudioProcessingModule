eb2ac298a9a6f584da244a15ec53da9336b1686e
NasterVill <skatrr@gmail.com>
2018-08-09 17:43:55 +0300

    Sound analysis was taken from pure Java LightningTuner

--- android/src/main/java/com/reactlibrary/RNAudioProcessingModule.java
+++ android/src/main/java/com/reactlibrary/RNAudioProcessingModule.java
@@ -19,11 +19,9 @@ import com.reactlibrary.fequency_tools.FrequencyDetector;
 import com.reactlibrary.fequency_tools.windows.HammingWindow;
 import android.util.Log;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Date;
-
-import static java.lang.Thread.sleep;
 
 public class RNAudioProcessingModule extends ReactContextBaseJavaModule implements Runnable {
     private static final int SAMPLE_RATE = 44100;
@@ -32,14 +30,106 @@ public class RNAudioProcessingModule extends ReactContextBaseJavaModule implemen
     private static final int MAX_FREQUENCY = 1300;
     private static final String FREQUENCY_DETECTED_EVENT_NAME = "FrequencyDetected";
     private static final String TAG = "RNAudioProcessingModule";
-    private static final float ALLOWED_FREQUENCY_DIFFERENCE = 1;
+
+    private final ReactApplicationContext reactContext;
+
+    private AudioRecord audioRecord;
+    private int buffSize = 0;
+    private boolean stopFlag = false;
+
+    public RNAudioProcessingModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("FREQUENCY_DETECTED_EVENT_NAME", RNAudioProcessingModule.FREQUENCY_DETECTED_EVENT_NAME);
+        return constants;
+    }
+
+    @Override
+    public String getName() {
+        return "RNAudioProcessingModule";
+    }
+
+    @ReactMethod
+    public void init() {
+        int minBufSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        if (minBufSize != AudioRecord.ERROR_BAD_VALUE && minBufSize != AudioRecord.ERROR) {
+            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, Math.max(DEFAULT_BUFF_SIZE, minBufSize * 4));
+            this.buffSize = Math.max(DEFAULT_BUFF_SIZE, minBufSize * 4);
+        }
+    }
+
+    @ReactMethod
+    public void stop() {
+        stopFlag = true;
+        audioRecord.stop();
+        audioRecord.release();
+    }
+
+    @ReactMethod
+    @Override
+    public void run() {
+        audioRecord.startRecording();
+        final int sampleRate = audioRecord.getSampleRate();
+        short[] mainBuffer = new short[this.buffSize];
+        FrequencyDetector detector = new FrequencyDetector();
+
+        do {
+            final int read = audioRecord.read(mainBuffer, 0, this.buffSize);
+            if (read > 0) {
+                float[] doubleBuff = shortToFloat(mainBuffer, read);
+
+                float frequency = detector.findFrequency(doubleBuff, sampleRate, MIN_FREQUENCY, MAX_FREQUENCY, new FFTCooleyTukey(), new HammingWindow());
+
+                WritableMap params = Arguments.createMap();
+                params.putDouble("frequency", frequency);
+
+                this.sendEvent(this.reactContext, RNAudioProcessingModule.FREQUENCY_DETECTED_EVENT_NAME, params);
+            }
+        } while (!stopFlag);
+    }
+
+    private float[] shortToFloat(short[] source, int length) {
+        length = (length > source.length) ? source.length : length;
+
+        float[] resultArray = new float[length];
+
+        for (int i = 0; i < length; i++) {
+            // The nominal range of ENCODING_PCM_FLOAT audio data is [-1.0, 1.0], but here we use
+            // ENCODING_PCM_16BIT, because ENCODING_PCM_FLOAT is supported only in API LOLLIPOP+ and higher
+            // so to work with float values, extended to double, we need to divide it
+            // by max 16-bit integer value
+            resultArray[i] = source[i] / 32768.0F;
+        }
+        return resultArray;
+    }
+}
+
+/*public class RNAudioProcessingModule extends ReactContextBaseJavaModule implements Runnable {
+    private static final int SAMPLE_RATE = 44100;
+    private static final int DEFAULT_BUFF_SIZE = 32768;
+    private static final int MIN_FREQUENCY = 40;
+    private static final int MAX_FREQUENCY = 1300;
+    private static final String FREQUENCY_DETECTED_EVENT_NAME = "FrequencyDetected";
+    private static final String TAG = "RNAudioProcessingModule";
 
     private final ReactApplicationContext reactContext;
 		
     private AudioRecord audioRecord = null;
     private int buffSize = 0;
     private boolean stopFlag = false;
-    private float lastComputedFrequency = 0;
 
    public RNAudioProcessingModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -89,7 +179,7 @@ public class RNAudioProcessingModule extends ReactContextBaseJavaModule implemen
         FrequencyDetector detector = new FrequencyDetector();
 
         do {
-            final int read = audioRecord.read(readBuffer, 0, this.buffSize, AudioRecord.READ_BLOCKING);
+            final int read = audioRecord.read(readBuffer, 0, this.buffSize, AudioRecord.READ_NON_BLOCKING);
             if (read > 0) {
                 float frequency = detector.findFrequency(readBuffer,
                                                         sampleRate,
@@ -97,15 +187,11 @@ public class RNAudioProcessingModule extends ReactContextBaseJavaModule implemen
                                                         RNAudioProcessingModule.MAX_FREQUENCY,
                                                         new FFTCooleyTukey(),
                                                         new HammingWindow());
-                if ((Math.abs(frequency - lastComputedFrequency) <= ALLOWED_FREQUENCY_DIFFERENCE) && frequency != 0) {
-                    WritableMap params = Arguments.createMap();
-                    params.putDouble("frequency", frequency);
 
-                    this.sendEvent(this.reactContext, RNAudioProcessingModule.FREQUENCY_DETECTED_EVENT_NAME, params);
-                }
-                if(frequency != 0) {
-                    lastComputedFrequency = frequency;
-                }
+                WritableMap params = Arguments.createMap();
+                params.putDouble("frequency", frequency);
+
+                this.sendEvent(this.reactContext, RNAudioProcessingModule.FREQUENCY_DETECTED_EVENT_NAME, params);
             }
         } while (!stopFlag);
     }
@@ -117,4 +203,4 @@ public class RNAudioProcessingModule extends ReactContextBaseJavaModule implemen
                 .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit(eventName, params);
     }
-}
+}*/
